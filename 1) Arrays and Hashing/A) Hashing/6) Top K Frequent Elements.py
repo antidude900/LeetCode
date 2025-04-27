014c25a8e673b0834e857fde966ac058ae179d49
Sambal Shrestha <splashofwater900@gmail.com>
2025-04-27 23:35:53 +0545

    Create 6) Top K Frequent Elements.py

---
+++ 1) Arrays and Hashing/A) Hashing/6) Top K Frequent Elements.py
@@ -0,0 +1,12 @@
+"""
+Approach:
+We just have to give the top k most frequent elements.
+For this: we first count the frequency of each element(using heap for faster access the previous value to increment or use the collector library)
+[in my case, i found that collector library decreases efficiency]
+Then, we just need to sort according to the frequency(in descending order for highest to come first) and then return the the first k of those
+I used various sorting algorithm such as tim sort, heap sort and heap sort with limit of k and here are the results:
+
+Heap sort with limit of k did well than simple heap sort but its efficiency was not quite consistent! 
+whereas tim sort with same efficiency as of the heap sort with limit of k is alot consistent!
+So i used tim sort but i will list the other two as well!
+"""
