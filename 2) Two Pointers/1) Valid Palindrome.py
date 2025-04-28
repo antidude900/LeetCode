2d7ba8df892536f5a4341ad7af70e3c408139874
Sambal Shrestha <splashofwater900@gmail.com>
2025-04-28 23:22:13 +0545

    Create 1) Valid Palindrome.py

---
+++ 2) Two Pointers/1) Valid Palindrome.py
@@ -0,0 +1,6 @@
+"""
+Approach:
+We have to check whether the string given is palindrome or not meaning if its same as its original string even when reversed.
+A palindrome only cares about alphanumeric characters! So to make the string more simpler we make a new string with only lowercase alphanumeric characters from the original one
+Then we use two pointers, one from the front and one from the back of the string to check whether the characters matches from both the side or not.
+"""
