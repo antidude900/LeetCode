fdf679e8862222b73d8500831d46a4ee148954f2
Sambal Shrestha <splashofwater900@gmail.com>
2025-05-02 19:40:15 +0545

    Update 8) Valid Suduko.py

--- 1) Arrays and Hashing/A) Hashing/8) Valid Suduko.py
+++ 1) Arrays and Hashing/A) Hashing/8) Valid Suduko.py
@@ -4,7 +4,8 @@ Here, we just have to see if number from 1 to 9 doesnt repeat in any row, col or
 
 For this we make a dict which key stores the index of the row/col/square it is(0-8) and its value stores the list of values in that row/col/square.
 As the check of whether a value already exists inside the list of values should be done, using set to store the list of values is optimal.
-Also for the square, we require 2 indicies for its location and as we are using the key for storing the index, we cannot use list for keys so we use tuples as keys for storing the 2 index.
+Also for the square, we require 2 indicies for its location and as we are using the key for storing the index, 
+we cannot use list for keys so we use tuples as keys for storing the 2 index.
 (as row and col are in a single dimension i.e row is in horizontal direction and col in vertical direction only but for the square,
 its goes only upto 3 steps in horizontal direction and after then it goes 1 step down in vertical direction and so on. 
 So, as squares move in 2 direction that is in 2D, we need two indices for its location.)
