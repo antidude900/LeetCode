9c66c7b6b1f3fd06090e099acd657d4158b47c0a
Sambal Shrestha <splashofwater900@gmail.com>
2025-05-31 21:53:20 +0545

    Update 5) Longest Substring Without Repeating Characters.py

--- 5) Sliding Windows/5) Longest Substring Without Repeating Characters.py
+++ 5) Sliding Windows/5) Longest Substring Without Repeating Characters.py
@@ -20,3 +20,18 @@ def lengthOfLongestSubstring(s: str) -> int:
             res = max(res,right-left+1)
         window[c]=right
     return res
+
+#Alternative (Using hash Set)
+def lengthOfLongestSubstring(s: str) -> int:
+        window = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in window:
+                window.remove(s[l])    #here we start removing characters from the left of the substring in the window until we removed the duplicate.
+                                       #it runs on the fact that every other characters to the left of the character whose duplicate is found is not needed
+                                       #as now we will be taking substring after the character whose duplicate is found
+                l += 1
+            window.add(s[r])
+            res = max(res, r - l + 1)
+        return res
