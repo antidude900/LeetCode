6fbb4a7058a2fc36526cc0ae04ce40b7c5583275
Sambal Shrestha <splashofwater900@gmail.com>
2025-06-15 21:27:31 +0545

    Update 3) Remove Duplicates from Sorted Array II.py

--- 2) Two Pointers/3) Remove Duplicates from Sorted Array II.py
+++ 2) Two Pointers/3) Remove Duplicates from Sorted Array II.py
@@ -8,6 +8,19 @@ But why need to keep count of each number when we can just see see 2 index befor
 """
 
 def removeDuplicates(nums: List[int]) -> int:
+    i = 2
+    for j in range(2,len(nums)):
+        if nums[j] != nums[i-2]: #why not j-2? its because if we take the index of the old array 'j' to see its 2nd before one, it might not work due to the improper tracking
+                                 #as we wont be checking 2nd before from where we got after the change 'i' but rather anywhere from the string which might result to 
+                                 #one element matching with its first occurance
+                                 #so to avoid that, we are seeing 2nd before from our new array. the number trying to come in the new array will be checked with the 2nd last element
+                                 #of the new array, as at the end the thing we want is that the i and i-2 to not match for the new array. 
+            nums[i] = nums[j]
+            i+=1
+    return i
+
+#as we dont need specially need the index 'j', we can just iterate through the num instead by index
+def removeDuplicates(nums: List[int]) -> int:
     l = 0
     for num in nums:
         if l < 2 or num != nums[l - 2]: #if the index is simply less than 2, duplicate checking will be valid fore sure. Else, we have to do the duplicate checking
