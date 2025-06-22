7457e8318f611f1c80e488b31c0b8be61827c224
Sambal Shrestha <splashofwater900@gmail.com>
2025-06-22 17:00:28 +0545

    Create 4) Maximum Twin Sum of a Linked List.py

---
+++ 6) Linked List/4) Maximum Twin Sum of a Linked List.py
@@ -0,0 +1,31 @@
+"""
+Approach:
+We have to find the maximum twin sum from the given linked list where twin for the ith indexed node is n-1-ith indexed node.
+One very simple way is just to store the values of the nodes in an array and then iterate through the ceil half of the array and find each's twin sum.
+
+But we have to make a new data structure for it. Can we do it without making one?
+Yes through, fast and slow pointer. As we know fast pointer moves double the speed of the slow pointer. 
+So, the slow pointer reaches the ceil half of the list when the fast pointer reaches the end.
+Another thing to understand is that the twins are just the corresponding same positions form first and last i.e 1 and n, 2 and n-1 and so on.
+So, what we do is start from the middle and thus one pointer goes to the left from the middle and one to the right each time pointer containing the twins.
+But to go the left from the middle, we cant simply do in simple linked list.so we have to reverse the left half to perform that operation.
+As we can see this method depends on the symmetry of the node. Hence it doesnt work for the odd size.
+"""
+
+def pairSum(self, head: Optional[ListNode]) -> int:
+    fast, slow = head,head
+    prev = None
+    while fast and fast.next: #reveresing until we reach middle
+        fast = fast.next.next
+        temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp
+
+    #reached middle and now iterating left and right from the middle getting the twins
+    res = 0
+    while (slow):
+        res = max(res,prev.val+slow.val)
+        prev = prev.next
+        slow = slow.next
+    return res
