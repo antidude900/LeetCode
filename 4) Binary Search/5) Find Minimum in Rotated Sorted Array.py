bfb0a3a7e5bb0f5ad9db74051dc04f6063c13501
Sambal Shrestha <splashofwater900@gmail.com>
2025-07-19 22:32:57 +0545

    Update 5) Find Minimum in Rotated Sorted Array.py

--- 4) Binary Search/5) Find Minimum in Rotated Sorted Array.py
+++ 4) Binary Search/5) Find Minimum in Rotated Sorted Array.py
@@ -4,7 +4,13 @@ Given a sorted array but rotated any no of time, we have to find the minimum ele
 For this we can take still use the sorted property. 
 For eg: If the mid is greater than the element towards its right, it means its the number from the rotated part as in a sorted array in ascending order, the before element cant be greater than the after one.
 So we dont need to see the left part of the mid because all of those part are rotated part too. So we only see the right half.
-If the mid is less than the element towards it, then it is in sorted order meaning its not of the rotated part. So we can be asuured that all the elements between will also be less.
+If the mid is less than the element towards its right, then it is in sorted order meaning its not of the rotated part. So we can be asuured that all the elements between will also be less.
+But why are we checking to its right? well because the elements are being shifted from the right.
+if we had done nums[m]>nums[l]: r = m, it will find the minimum for the rotated part which infact wont have the minimum. 
+so u might think l=m to remove the rotated part but what if the array is rotated back to its fully sorted form. then instead of eliminating the roatated part, you are eliminating the part having minimum!
+Hence its always to check from the right as it will start finding minimum from the non rotated part and also if not rotated at all it is shifting towards the left surely giving us the minimum.
+To conclude, its better to check from right and shift towards left rather than checking from left and shifting towards the left
+(shifing towards the left is always a good idea as the array is sorted and hence the minimum will be at its left)
 So we dont have to see the right half and we just see the left part(because we might have took mid between in the sorted portion meaning there can me more less ones in the left)
 """
 
