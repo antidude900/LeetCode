86017f29843935f26a01f304a706e448a6bbf8ce
Sambal Shrestha <splashofwater900@gmail.com>
2025-07-22 18:47:58 +0545

    Update 3) Find Pivot Index.py

--- 1) Arrays and Hashing/B) Prefix Sum/3) Find Pivot Index.py
+++ 1) Arrays and Hashing/B) Prefix Sum/3) Find Pivot Index.py
@@ -8,18 +8,18 @@ is equal to
 the sum of remaning elements to the right i.e prefix_sum[index_of_last_element]-prefix_sum[index_observing_rn] 
 """
 
-def pivotIndex(nums: List[int]) -> int:
-    n=len(nums)
-    prefix_sum = [0]*(n+1)
+def pivotIndex(self, nums: List[int]) -> int:
+    prefix  = 0
+    prefixSum = []
     
-    prefix = 0
-    for i in range(len(nums)):
-        prefix += nums[i]
-        prefix_sum[i] = prefix
+    for num in nums:
+        prefix+=num
+        prefixSum.append(prefix)
         
-    for i in range(n):
-        left = (prefix_sum[i-1] if i>0 else 0)
-        right = (prefix_sum[n-1]-prefix_sum[i] if i<n-1 else 0)
-        if left == right:
+    for i in range(len(prefixSum)):
+        left = prefixSum[i-1] if i>0 else 0
+        right = prefixSum[-1]-prefixSum[i]
+    
+        if left==right:
             return i
-    return -1    #return -1 if theres no pivot Index
+    return -1
