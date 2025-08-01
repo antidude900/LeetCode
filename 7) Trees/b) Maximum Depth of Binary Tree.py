1d0bc3d5c1fe9a5452821bb5b648fc0f0db6ea21
Sambal Shrestha <splashofwater900@gmail.com>
2025-08-01 22:53:45 +0545

    Update b) Maximum Depth of Binary Tree.py

--- 7) Trees/b) Maximum Depth of Binary Tree.py
+++ 7) Trees/b) Maximum Depth of Binary Tree.py
@@ -7,7 +7,7 @@ hence we apply the same search for the maxdepth for tits lef and right sub tree
 """
 
 def maxDepth(self, root: Optional[TreeNode]) -> int:
-    if not root: #if there's no node, we cant add anything to the dept
+    if not root: #if there's no node, we cant add anything to the depth
         return 0
     
     return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
