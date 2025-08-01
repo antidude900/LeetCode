df7015fddd1994fc9a67d86eda4b0583578e9a4d
Sambal Shrestha <splashofwater900@gmail.com>
2025-08-01 22:51:33 +0545

    Create b) Maximum Depth of Binary Tree.py

---
+++ 7) Trees/b) Maximum Depth of Binary Tree.py
@@ -0,0 +1,13 @@
+"""
+Approach:
+We are given a binary tree and we have to find the max depth of the tree(here we assume depth of a node to be 1 in its onw level)
+[Note: A tree's max depth and height will be the same]
+We can use recursion to simplify the process. We first take 1 as the depth of its own self and add the depth of its left or right sub tree either one who is the biggest
+hence we apply the same search for the maxdepth for tits lef and right sub tree
+"""
+
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if not root: #if there's no node, we cant add anything to the dept
+        return 0
+    
+    return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
