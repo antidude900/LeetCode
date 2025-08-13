4ee988c93675166a5d22d802c4004766fa87db4b
Sambal Shrestha <splashofwater900@gmail.com>
2025-08-13 19:57:32 +0545

    Update 7) Time Based Key-Value Store.py

--- 4) Binary Search/7) Time Based Key-Value Store.py
+++ 4) Binary Search/7) Time Based Key-Value Store.py
@@ -11,35 +11,35 @@ As there's not clever trick in this question and it is straight forward, nothing
 class TimeMap:
 
     def __init__(self):
-        self.map = defaultdict(list)
+        self.timestamps = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.map[key].append([timestamp,value])
+        self.timestamps[key].append([timestamp,value])
 
     def get(self, key: str, timestamp: int) -> str:
-        timestamps = self.map[key]
-        if not timestamps:
-            return ""
+        vals = self.timestamps[key]
 
-        #timestamps can get less than the previous one irl. so the next one wont be smaller than the previous one. so the smallest timestamp is the first one and the biggest one is the last one
+        #timestamp can get less than the previous one irl. so the next one wont be smaller than the previous one. so the smallest timestamp is the first one and the biggest one is the last one
         #so if the given timestamp is less than the first one, there's no solution.
-        if timestamp < timestamps[0][0]: 
+        if not vals or timestamp<vals[0][0]:
             return ""
 
         #and if the given timestamp is greater than the last one, the nearest smaller one is the last one
-        if timestamp > timestamps[-1][0]: 
-            return timestamps[-1][1]
+        if timestamp > vals[-1][0]: 
+            return vals[-1][1]
 
-        left, right = 0, len(timestamps)-1
+        l, r = 0, len(vals)-1
         res = ""
         
-        while left <= right:
-            mid = left+(right - left) // 2
-            if timestamp == timestamps[mid][0]:
-                return timestamps[mid][1]
-            elif timestamp > timestamps[mid][0]:
-                res = timestamps[mid][1] #as this timestamp is less than the timestamp we require, it might be the timestamp_prev result value
-                left = mid + 1
+        while l <= r:
+            m = l+(r - l) // 2
+            if timestamp == vals[m][0]:
+                return vals[m][1]
+                
+            elif timestamp > vals[m][0]:
+                l = m+1
+                res = vals[m][1] #as this timestamp is less than the timestamp we require, it might be the timestamp_prev result value
             else:
-                right = mid -1
+                r = m-1
+                
         return res    #at the end, the latest value which is less than the timestamp will be final timestamp_prev for the output(if we dont find the exact timestamp)
