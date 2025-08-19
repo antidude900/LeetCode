411a1f8fbd466550de69470ca08adcd7440bf92a
Sambal Shrestha <splashofwater900@gmail.com>
2025-08-19 15:11:26 +0545

    Update Kth Smallest Element in a BST.py

--- 7) Trees/Kth Smallest Element in a BST.py
+++ 7) Trees/Kth Smallest Element in a BST.py
@@ -18,7 +18,9 @@ def kthSmallest(root: Optional[TreeNode], k: int) -> int:
         nonlocal res,k    #nested function always creates a locat variable for itself. so to avoid that, we use give it nonlocal scope    
         k-=1    #the current node is of the smallest.  so deduct the value of k
         if k == 0:    #if k = 0, this is the node we are looking for (If it was 0-indexed, we have to set k=k+1 to bring the count back to 1 indexed
-            res = root.val    
+            res = root.val    #if we update it oustide, then it gets replaced by the right of its parent node as k-=1 makes it k=-1 and thus k!=0 so will go towards dfs(root.right)
+                              #then you might think of fix of k<=0 but still it will get replaced by its parent node as "if k <= 0: return" stops the execution after it 
+                              #but still the update of res before "if k <= 0: return" will run. so res will be replaced by its parent node though the node rn is the answer
             return 
             
         dfs(root.right)    #else we go to its right subtree
